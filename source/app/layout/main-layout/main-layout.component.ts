ca4f436483b13e6825df577a8e992d0f755b6878
https-richardy <code.richardy@gmail.com>
2024-10-27 13:14:01 -0300

    adds a floating button (FAB) that displays AI recommendations in the main layout.

--- source/app/layout/main-layout/main-layout.component.ts
+++ source/app/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
+import { AiRecommendationFabComponent } from "../../components/ai-recommendation-fab/ai-recommendation-fab.component";
 
 @Component({
     selector: 'main-layout',
     standalone: true,
-    imports: [NavigationComponent],
+    imports: [NavigationComponent, AiRecommendationFabComponent],
     templateUrl: './main-layout.component.html'
 })
 export class MainLayoutComponent {
