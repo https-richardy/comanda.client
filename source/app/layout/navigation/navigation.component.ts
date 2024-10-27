f6f9e0bbf48455bbc5697e8836231e415633a79e
https-richardy <code.richardy@gmail.com>
2024-10-27 12:30:16 -0300

    refactors the navigation to use a reusable component (`navigation-item`) for each item, simplifying the code and making it easier to maintain.

--- source/app/layout/navigation/navigation.component.ts
+++ source/app/layout/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationItemComponent } from "../navigation-item/navigation-item.component";
 
 @Component({
     selector: 'navigation',
     standalone: true,
-    imports: [],
+    imports: [ NavigationItemComponent ],
     templateUrl: './navigation.component.html',
 })
 export class NavigationComponent {
