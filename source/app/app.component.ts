cd9cb34a17494a3d89e941c18307cabec229aac7
https-richardy <code.richardy@gmail.com>
2024-10-27 18:38:26 -0300

    adds a product gallery component to the home page, displaying a view of the available products.

--- source/app/app.component.ts
+++ source/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainLayoutComponent } from "./layout/main-layout/main-layout.component";
+import { ProductGalleryComponent } from "./components/product-gallery/product-gallery.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MainLayoutComponent],
+    imports: [
+        RouterOutlet,
+        MainLayoutComponent,
+        ProductGalleryComponent
+    ],
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    title = 'comanda.client';
+    title = "comanda.client"
 }
