ea447b8c5280fa4121b19eccc17eff9b828fe12e
https-richardy <code.richardy@gmail.com>
2024-10-27 11:20:39 -0300

    it implements the Claim class to encapsulate the type and value of claims used in the authentication system. This allows for a consistent and organized representation of claims, facilitating the management and manipulation of authorization information.

---
+++ source/app/modules/authorization/claims.ts
@@ -0,0 +1,9 @@
+export class Claim {
+    public type: string;
+    public value: string;
+
+    public constructor(type: string, value: string) {
+        this.type = type;
+        this.value = value;
+    }
+}
