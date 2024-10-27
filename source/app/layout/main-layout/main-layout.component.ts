bdb5abc22eb97b493b90d97256c40c81b43e5097
https-richardy <code.richardy@gmail.com>
2024-10-27 19:36:07 -0300

    adds the search component (`SearchComponent`) to the main layout (`MainLayoutComponent`), allowing users to search for products within the application.

--- source/app/layout/main-layout/main-layout.component.ts
+++ source/app/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
 import { AiRecommendationFabComponent } from "../../components/ai-recommendation-fab/ai-recommendation-fab.component";
+import { SearchComponent } from "../../components/search/search.component";
 
 @Component({
     selector: 'main-layout',
     standalone: true,
-    imports: [NavigationComponent, AiRecommendationFabComponent],
+    imports: [
+        NavigationComponent,
+        AiRecommendationFabComponent,
+        SearchComponent
+    ],
     templateUrl: './main-layout.component.html'
 })
 export class MainLayoutComponent {
