e3d70ea9332bd443f2434d45ec8cdb725ed4fcdf
https-richardy <code.richardy@gmail.com>
2024-10-28 20:46:18 -0300

    this commit adds the `DialogProviderComponent` component to the application's main layout, allowing the application to manage modal dialogs centrally.

--- source/app/layout/main-layout/main-layout.component.ts
+++ source/app/layout/main-layout/main-layout.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
 import { AiRecommendationFabComponent } from "../../components/ai-recommendation-fab/ai-recommendation-fab.component";
 import { SearchComponent } from "../../components/search/search.component";
+import { DialogProviderComponent } from "../../components/dialog-provider/dialog-provider.component";
 
 @Component({
     selector: 'main-layout',
     standalone: true,
     imports: [
-        NavigationComponent,
-        AiRecommendationFabComponent,
-        SearchComponent
-    ],
+    NavigationComponent,
+    AiRecommendationFabComponent,
+    SearchComponent,
+    DialogProviderComponent
+],
     templateUrl: './main-layout.component.html'
 })
 export class MainLayoutComponent {
