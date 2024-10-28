b2d391f36913e9f1deccd2a27cf42606037ce4e7
https-richardy <code.richardy@gmail.com>
2024-10-28 14:37:38 -0300

    implements the home page (`HomePageComponent`) as the application's default route, displayed when the user accesses the URL `/`. The route to the profile (`/profile`) remains available.

--- source/app/app.routes.ts
+++ source/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+
 import { ProfilePageComponent } from './pages/profile/profile-page.component';
+import { HomePageComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: "profile", component: ProfilePageComponent }
+    { path: "", component: HomePageComponent },
+    { path: "profile", component: ProfilePageComponent },
 ];
