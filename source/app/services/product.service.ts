d4e0a54ff2c1c260e5f590f67a09bc8fd372fe13
https-richardy <code.richardy@gmail.com>
2024-10-28 19:19:00 -0300

    this commit introduces API_BASE_URL injection for the base url. Using dependency injection to receive the API base URL, ensuring that the product service uses the correct configuration according to the environment.

--- source/app/services/product.service.ts
+++ source/app/services/product.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { IProductService } from './interfaces/product.service.interface';
 import { Response } from '../payloads/responses/response';
 import { Product } from '../models/product.model';
 import { Pagination } from '../payloads/responses/pagination';
+import { API_BASE_URL } from '../app.tokens';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService implements IProductService {
     private readonly httpClient: HttpClient;
-    private readonly baseAddress: string = "http://localhost:5168/api/products";
+    private readonly baseAddress: string = `${inject(API_BASE_URL)}api/products`;
 
     public constructor(httpClient: HttpClient) {
         this.httpClient = httpClient;
