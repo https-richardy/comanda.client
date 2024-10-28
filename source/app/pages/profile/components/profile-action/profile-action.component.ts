b071c220b3e93bdd37038adc5908c7d19279f866
https-richardy <code.richardy@gmail.com>
2024-10-28 16:08:02 -0300

    style: format the indentation to 4 spaces

--- source/app/pages/profile/components/profile-action/profile-action.component.ts
+++ source/app/pages/profile/components/profile-action/profile-action.component.ts
@@ -3,13 +3,13 @@ import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'profile-action',
-  standalone: true,
-  imports: [ CommonModule, RouterModule ],
-  templateUrl: './profile-action.component.html',
+    selector: 'profile-action',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './profile-action.component.html',
 })
 export class ProfileActionComponent {
-  @Input() public label: string = "";
-  @Input() public icon: string = "";
-  @Input() public link: string = "";
+    @Input() public label: string = "";
+    @Input() public icon: string = "";
+    @Input() public link: string = "";
 }
