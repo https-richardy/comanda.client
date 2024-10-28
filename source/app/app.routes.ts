277a5c86e854a3fd2e7eb1861cf6b47679e83a66
https-richardy <code.richardy@gmail.com>
2024-10-28 14:23:11 -0300

    adds a new `/profile` route that goes to the `ProfilePageComponent` component.

--- source/app/app.routes.ts
+++ source/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "profile", component: ProfilePageComponent }
+];
