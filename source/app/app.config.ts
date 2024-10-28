421fbe7f02124f833333cadd2cdcad9a72e45c58
https-richardy <code.richardy@gmail.com>
2024-10-28 19:13:12 -0300

    adds the base API configuration `API_BASE_URL` to the `app.config.ts` file, using `provide: API_BASE_URL` and `useValue: “http://localhost:5168”`. This allows the application to connect to the local API under development.

--- source/app/app.config.ts
+++ source/app/app.config.ts
@@ -3,14 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { API_BASE_URL } from './app.tokens';
 
 export class ApplicationConfigBuilder {
     public static build(): ApplicationConfig {
         return {
             providers: [
+                { provide: API_BASE_URL, useValue: "http://localhost:5168" },
+
                 provideZoneChangeDetection({ eventCoalescing: true }),
                 provideRouter(routes),
-                provideHttpClient()
+                provideHttpClient(),
             ]
         }
     }
