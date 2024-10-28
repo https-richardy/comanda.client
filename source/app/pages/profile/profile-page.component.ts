7b80d3f8385dd94214e27c5038f94ecf2aef130a
https-richardy <code.richardy@gmail.com>
2024-10-28 16:13:21 -0300

    this commit removes the display logic from profile-page.component.html and centralizes the logic in the new component.

--- source/app/pages/profile/profile-page.component.ts
+++ source/app/pages/profile/profile-page.component.ts
@@ -1,19 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 import { MainLayoutComponent } from "../../layout/main-layout/main-layout.component";
 import { NavigationComponent } from "../../layout/navigation/navigation.component";
 import { ProfileActionComponent } from './components/profile-action/profile-action.component';
 import { ProfileActionIcons } from './components/profile-action/icons/profile-action.icons';
+import { ProfileInfoComponent } from "./components/profile-info/profile-info.component";
 
 @Component({
     selector: 'app-profile-page',
     standalone: true,
     imports: [
-        CommonModule,
-        MainLayoutComponent,
-        NavigationComponent,
-        ProfileActionComponent
-    ],
+    CommonModule,
+    MainLayoutComponent,
+    NavigationComponent,
+    ProfileActionComponent,
+    ProfileInfoComponent
+],
     templateUrl: './profile-page.component.html',
 })
 export class ProfilePageComponent {
@@ -23,20 +26,4 @@ export class ProfilePageComponent {
         email: 'richard@garcia.com',
         status: 'Ativo'
     };
-
-    public emailNotifications = false;
-    public darkMode = false;
-    public twoFactorAuth = false;
-
-    public toggleEmailNotifications() {
-        this.emailNotifications = !this.emailNotifications;
-    }
-
-    public toggleDarkMode() {
-        this.darkMode = !this.darkMode;
-    }
-
-    public toggleTwoFactorAuth() {
-        this.twoFactorAuth = !this.twoFactorAuth;
-    }
 }
