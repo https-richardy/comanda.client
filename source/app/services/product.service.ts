b43f32d9c07dd048125543aaf407c5ddac50c6fe
https-richardy <code.richardy@gmail.com>
2024-10-28 17:50:13 -0300

    implements integration with the local API to obtain products.

--- source/app/services/product.service.ts
+++ source/app/services/product.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { IProductService } from './interfaces/product.service.interface';
+import { Response } from '../payloads/responses/response';
 import { Product } from '../models/product.model';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService implements IProductService {
     private readonly httpClient: HttpClient;
-    private readonly baseAddress: string = 'https://fakestoreapi.com/products';
+    private readonly baseAddress: string = "http://localhost:5168/api/products";
 
     public constructor(httpClient: HttpClient) {
         this.httpClient = httpClient;
@@ -18,6 +19,10 @@ export class ProductService implements IProductService {
     }
 
     public getProducts(): Observable<Product[]> {
-        return this.httpClient.get<Product[]>(this.baseAddress);
+        var response = this.httpClient.get<Response<Product[]>>(this.baseAddress).pipe(
+            map((response) => response.data || [])
+        );
+
+        return response;
     }
 }
