0c429aa3e13a230501e4cc88f400a3f3211faaa5
https-richardy <code.richardy@gmail.com>
2024-10-28 19:32:04 -0300

    remove the `API_STATIC_URL` from the application configuration.

--- source/app/app.config.ts
+++ source/app/app.config.ts
@@ -3,14 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { API_BASE_URL, API_STATIC_URL } from './app.tokens';
+import { API_BASE_URL } from './app.tokens';
 
 export class ApplicationConfigBuilder {
     public static build(): ApplicationConfig {
         return {
             providers: [
                 { provide: API_BASE_URL, useValue: "http://localhost:5168/" },
-                { provide: API_STATIC_URL, useValue: "http://localhost:5168/uploads/" },
 
                 provideZoneChangeDetection({ eventCoalescing: true }),
                 provideRouter(routes),
--- source/app/app.tokens.ts
+++ source/app/app.tokens.ts
@@ -1,4 +1,3 @@
 import { InjectionToken } from "@angular/core";
 
 export const API_BASE_URL = new InjectionToken<string>("API_BASE_URL");
-export const API_STATIC_URL = new InjectionToken<string>("API_STATIC_URL")
