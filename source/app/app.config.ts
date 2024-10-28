31603e233a5c73d3ea69b4937eafd69317bede4a
https-richardy <code.richardy@gmail.com>
2024-10-28 17:56:17 -0300

    adds `HttpClient` to the application's main module to allow HTTP requests to be made. This addition will allow interaction with external APIs and services

--- source/app/app.config.ts
+++ source/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export class ApplicationConfigBuilder {
     public static build(): ApplicationConfig {
         return {
             providers: [
                 provideZoneChangeDetection({ eventCoalescing: true }),
-                provideRouter(routes)
+                provideRouter(routes),
+                provideHttpClient()
             ]
         }
     }
