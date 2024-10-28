3f6a5a3dd917795c696ba8cc1f2387b4f00c5326
https-richardy <code.richardy@gmail.com>
2024-10-28 14:21:43 -0300

    this commit introduces the profile management page.

---
+++ source/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MainLayoutComponent } from "../../layout/main-layout/main-layout.component";
+
+@Component({
+    selector: 'app-profile-page',
+    standalone: true,
+    imports: [CommonModule, MainLayoutComponent],
+    templateUrl: './profile-page.component.html',
+})
+export class ProfilePageComponent {
+    public user = {
+        name: 'Richard Garcia',
+        email: 'richard@garcia.com',
+        status: 'Ativo'
+    };
+
+    public emailNotifications = false;
+    public darkMode = false;
+    public twoFactorAuth = false;
+
+    public toggleEmailNotifications() {
+        this.emailNotifications = !this.emailNotifications;
+    }
+
+    public toggleDarkMode() {
+        this.darkMode = !this.darkMode;
+    }
+
+    public toggleTwoFactorAuth() {
+        this.twoFactorAuth = !this.twoFactorAuth;
+    }
+}
