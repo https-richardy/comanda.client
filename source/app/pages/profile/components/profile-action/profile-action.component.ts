aaee2c0824927d0a715b6efcda9ab7ec8ce267f1
https-richardy <code.richardy@gmail.com>
2024-10-28 16:03:56 -0300

    implements a new component to display profile actions, with icon, label and link.

---
+++ source/app/pages/profile/components/profile-action/profile-action.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'profile-action',
+  standalone: true,
+  imports: [ CommonModule, RouterModule ],
+  templateUrl: './profile-action.component.html',
+})
+export class ProfileActionComponent {
+  @Input() public label: string = "";
+  @Input() public icon: string = "";
+  @Input() public link: string = "";
+}
