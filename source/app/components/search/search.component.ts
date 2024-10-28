514d950280e5f72cc2aa43d58a278cbcc3bfb8ff
https-richardy <code.richardy@gmail.com>
2024-10-28 18:41:18 -0300

    adds synchronization of searchTerm with ngModel and search when pressing Enter

--- source/app/components/search/search.component.ts
+++ source/app/components/search/search.component.ts
@@ -1,20 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'search',
     standalone: true,
-    imports: [ CommonModule ],
+    providers: [ ProductService ],
+    imports: [ CommonModule, FormsModule ],
     templateUrl: './search.component.html',
 })
 export class SearchComponent {
+    public searchTerm: string = "";
     public isFocused: boolean = false;
 
+    private readonly productService: ProductService;
+    @Output() public onSearch = new EventEmitter<Product[]>();
+
+    public constructor(productService: ProductService) {
+        this.productService = productService;
+    }
+
     public handleOnFocus(): void {
-      this.isFocused = true;
+        this.isFocused = true;
     }
 
     public handleOnBlur(): void {
-      this.isFocused = false;
+        this.isFocused = false;
+    }
+
+    public performSearch(): void {
+        console.log("Realizando pesquisa: ", this.searchTerm);
+        this.onSearch.emit([])
     }
 }
