7634730df0a3769f0ba25153573dc5569ff40616
https-richardy <code.richardy@gmail.com>
2024-10-28 19:19:49 -0300

    adds the base address (API_BASE_URL) to the URL of the product image to access the server's static files.

--- source/app/components/product-card/product-card.component.ts
+++ source/app/components/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
+import { API_BASE_URL } from '../../app.tokens';
 
 @Component({
     selector: 'product-card',
@@ -9,6 +10,8 @@ import { Product } from '../../models/product.model';
     templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
+    public readonly baseAddress: string = inject(API_BASE_URL);
+
     @Input() public product!: Product;
     @Output() public onCartAddClick = new EventEmitter<Product>();
 
