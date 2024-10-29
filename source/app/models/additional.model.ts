ef92f61a8d81675e346b53b4daffb9b600d4fe7e
https-richardy <code.richardy@gmail.com>
2024-10-29 14:52:39 -0300

    adds name field to Additional model

--- source/app/models/additional.model.ts
+++ source/app/models/additional.model.ts
@@ -2,11 +2,13 @@ import { Category } from "./category.model";
 
 export class Additional {
     public id: number;
+    public name: string;
     public price: number;
     public category: Category;
 
-    public constructor(id: number, price: number, category: Category) {
+    public constructor(id: number, name: string, price: number, category: Category) {
         this.id = id;
+        this.name = name;
         this.price = price;
         this.category = category;
     }
