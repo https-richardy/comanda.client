6067aec125bc9b08a8cafa901a95cfb9e62c6dce
https-richardy <code.richardy@gmail.com>
2024-10-29 13:04:51 -0300

    adds a product customization dialog that allows users to customize products before adding them to the cart.

--- source/app/components/product-card/product-card.component.ts
+++ source/app/components/product-card/product-card.component.ts
@@ -2,6 +2,9 @@ import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
 import { API_BASE_URL } from '../../app.tokens';
+import { Personalization } from '../../payloads/requests/cart-payloads/personalization';
+import { DialogService } from '../../services/dialog.service';
+import { ProductCustomizationDialogComponent } from '../dialogs/product-customization-dialog/product-customization-dialog.component';
 
 @Component({
     selector: 'product-card',
@@ -11,12 +14,28 @@ import { API_BASE_URL } from '../../app.tokens';
 })
 export class ProductCardComponent {
     public readonly baseAddress: string = inject(API_BASE_URL);
+    private readonly dialogService: DialogService
 
     @Input() public product!: Product;
-    @Output() public onCartAddClick = new EventEmitter<Product>();
+    @Output() public onCartAddClick = new EventEmitter<{ product: Product, personalization: Personalization }>();
+
+    public constructor(dialogService: DialogService) {
+        this.dialogService = dialogService;
+    }
 
     public handleOnCartAddClick(): void {
-        this.onCartAddClick.emit(this.product);
+        const dialogRef = this.dialogService.open(ProductCustomizationDialogComponent, {
+            data: {
+                product: this.product
+            },
+            showCloseButton: true,
+            closeOnBackdrop: true
+        });
+
+        dialogRef.instance.onSubmit.subscribe((personalization) => {
+            this.dialogService.close();
+            this.onCartAddClick.emit({ product: this.product, personalization });
+        });
     }
 }
 
