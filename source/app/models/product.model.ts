655e569e110c78a3f8b2ad9fecdabe3039359cb3
https-richardy <code.richardy@gmail.com>
2024-10-29 11:51:22 -0300

    adds the `category` property of type `Category` and the `ingredients` property of type `Ingredient[]` to the `Product` class, allowing a product to be associated with a category and its ingredients to be included.

--- source/app/models/product.model.ts
+++ source/app/models/product.model.ts
@@ -1,3 +1,6 @@
+import { Category } from "./category.model";
+import { Ingredient } from "./ingredient.model";
+
 export class Product {
     public id: number;
     public title: string;
@@ -5,11 +8,22 @@ export class Product {
     public image: string;
     public description: string;
 
-    public constructor(id: number, title: string, price: number, image: string, description: string) {
+    public category: Category;
+    public ingredients: Ingredient[] = [];
+
+    public constructor(
+        id: number,
+        title: string,
+        price: number,
+        image: string,
+        description: string,
+        category: Category
+    ) {
         this.id = id;
         this.title = title;
         this.price = price;
         this.image = image;
         this.description = description;
+        this.category = category;
     }
 }
