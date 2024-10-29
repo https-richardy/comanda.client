05dc5c061ea364634b62c0d957259db1f22879f8
https-richardy <code.richardy@gmail.com>
2024-10-29 15:15:15 -0300

    style: set the indentation to 4 spaces.

--- source/app/components/dialogs/product-customization-dialog/components/ingredient/ingredient.component.ts
+++ source/app/components/dialogs/product-customization-dialog/components/ingredient/ingredient.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Ingredient } from '../../../../../models/ingredient.model';
 
 @Component({
-  selector: 'ingredient',
-  standalone: true,
-  imports: [ CommonModule ],
-  templateUrl: './ingredient.component.html'
+    selector: 'ingredient',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './ingredient.component.html'
 })
 export class IngredientComponent {
-  @Input() public ingredient!: Ingredient;
+    @Input() public ingredient!: Ingredient;
 }
