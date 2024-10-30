ddfa21d234db623939a50e57ef1fb1867ab8cba0
https-richardy <code.richardy@gmail.com>
2024-10-30 00:04:40 -0300

    adds a new component to the cart navigation item, which displays a cart icon, the number of items in the cart

---
+++ source/app/layout/cart-navigation-item/cart-navigation-item.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+    selector: 'cart-navigation-item',
+    standalone: true,
+    imports: [ CommonModule, RouterModule ],
+    templateUrl: './cart-navigation-item.component.html',
+})
+export class CartNavigationItemComponent implements OnInit {
+    public route: string = '/cart';
+    public badgeCount: number = 0;
+
+    private readonly cartService: CartService;
+
+    public constructor(cartService: CartService) {
+        this.cartService = cartService;
+    }
+
+    public ngOnInit(): void {
+        this.cartService.getCart().subscribe(cart => {
+            this.badgeCount = cart.items.length;
+        });
+    }
+}
--- source/app/layout/navigation/navigation.component.ts
+++ source/app/layout/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationItemComponent } from "../navigation-item/navigation-item.component";
+import { CartNavigationItemComponent } from "../cart-navigation-item/cart-navigation-item.component";
 
 @Component({
     selector: 'navigation',
     standalone: true,
-    imports: [ NavigationItemComponent ],
+    imports: [NavigationItemComponent, CartNavigationItemComponent],
     templateUrl: './navigation.component.html',
 })
 export class NavigationComponent {
