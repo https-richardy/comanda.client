8ba3a0807b18736e2b7cfa6b44e24d81e7a4ea75
https-richardy <code.richardy@gmail.com>
2024-10-30 11:15:05 -0300

    adds the functionality of adding products to the cart via the product card component. The user can now customize the product before adding it to the cart, and the cart is updated with the customization information.

--- source/app/components/product-card/product-card.component.ts
+++ source/app/components/product-card/product-card.component.ts
@@ -5,22 +5,25 @@ import { API_BASE_URL } from '../../app.tokens';
 import { Personalization } from '../../payloads/requests/cart-payloads/personalization';
 import { DialogService } from '../../services/dialog.service';
 import { ProductCustomizationDialogComponent } from '../dialogs/product-customization-dialog/product-customization-dialog.component';
+import { CartService } from '../../services/cart.service';
 
 @Component({
     selector: 'product-card',
     standalone: true,
-    imports: [ CommonModule ],
+    imports: [CommonModule],
     templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
     public readonly baseAddress: string = inject(API_BASE_URL);
     private readonly dialogService: DialogService
+    private readonly cartService: CartService;
 
     @Input() public product!: Product;
     @Output() public onCartAddClick = new EventEmitter<{ product: Product, personalization: Personalization }>();
 
-    public constructor(dialogService: DialogService) {
+    public constructor(dialogService: DialogService, cartService: CartService) {
         this.dialogService = dialogService;
+        this.cartService = cartService;
     }
 
     public handleOnCartAddClick(): void {
@@ -34,7 +37,23 @@ export class ProductCardComponent {
 
         dialogRef.instance.onSubmit.subscribe((personalization) => {
             this.dialogService.close();
-            this.onCartAddClick.emit({ product: this.product, personalization });
+            this.cartService
+                .addItem({
+                    productId: this.product.id,
+                    quantity: 1,
+                    ingredientsIdsToRemove: personalization.ingrendientsIdsToRemove,
+                    additionals: personalization.additionals
+                })
+                .subscribe(cart => {
+                    if (cart) {
+                        console.log('Carrinho atualizado:', cart);
+                        this.onCartAddClick.emit({ product: this.product, personalization });
+                    } else {
+                        console.error('Erro ao atualizar o carrinho.');
+                    }
+                }, error => {
+                    console.error('Erro ao adicionar item ao carrinho:', error);
+                });
         });
     }
 }
