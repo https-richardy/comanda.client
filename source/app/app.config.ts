8702de009393b0deab2feb96c22b7e295f4b4879
https-richardy <code.richardy@gmail.com>
2024-10-29 22:09:10 -0300

    implements an authentication interceptor to add the authentication token to all HTTP requests. The interceptor is configured in the application's configuration module

--- source/app/app.config.ts
+++ source/app/app.config.ts
@@ -2,14 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { API_BASE_URL } from './app.tokens';
+import { AuthenticationInterceptor } from './interceptors/authentication.interceptor';
 
 export class ApplicationConfigBuilder {
     public static build(): ApplicationConfig {
         return {
             providers: [
                 { provide: API_BASE_URL, useValue: "http://localhost:5168/" },
+                { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
 
                 provideZoneChangeDetection({ eventCoalescing: true }),
                 provideRouter(routes),
