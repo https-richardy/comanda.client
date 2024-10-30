c520623d3f8be67b016d10d38cf1bdb1c2b904be
https-richardy <code.richardy@gmail.com>
2024-10-29 23:46:45 -0300

    adds the `@Injectable()` annotation to the authentication interceptor to allow dependency injection, making the interceptor available to Angular.

--- source/app/interceptors/authentication.interceptor.ts
+++ source/app/interceptors/authentication.interceptor.ts
@@ -1,6 +1,8 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
+@Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
     public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = localStorage.getItem('authenticationToken');
