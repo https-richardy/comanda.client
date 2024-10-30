b0f62a4addaf929f54e0b967ce56acd977170e5a
https-richardy <code.richardy@gmail.com>
2024-10-29 22:06:27 -0300

    creates a new `HTTP_INTERCEPTORS` token to register custom HTTP interceptors in the application. This will allow the implementation of interceptors to add functionalities such as authentication and error handling.

--- source/app/app.tokens.ts
+++ source/app/app.tokens.ts
@@ -1,3 +1,5 @@
+import { HttpInterceptor } from "@angular/common/http";
 import { InjectionToken } from "@angular/core";
 
 export const API_BASE_URL = new InjectionToken<string>("API_BASE_URL");
+export const HTTP_INTERCEPTORS = new InjectionToken<HttpInterceptor>("HTTP_INTERCEPTORS")
