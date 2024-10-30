454e79a666754399708aa0d230f641d245257132
https-richardy <code.richardy@gmail.com>
2024-10-29 22:05:59 -0300

    adds an interceptor to add the authentication token to the header of HTTP requests.

---
+++ source/app/interceptors/authentication.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class AuthenticationInterceptor implements HttpInterceptor {
+    public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token = localStorage.getItem('authenticationToken');
+
+        if (token) {
+            const authenticatedRequest = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${token}`)
+            });
+
+            return next.handle(authenticatedRequest);
+        }
+
+        return next.handle(req);
+    }
+}
