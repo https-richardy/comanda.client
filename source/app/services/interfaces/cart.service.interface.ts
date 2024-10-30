616d8a3c943d8372753d852be3eec65a9dde3cf3
https-richardy <code.richardy@gmail.com>
2024-10-30 00:10:25 -0300

    adds a new contract to the cart service interface, the contract defines the functionality of adding an item to the cart, receiving an `InsertProductIntoCartRequest` object and returning the updated cart.

--- source/app/services/interfaces/cart.service.interface.ts
+++ source/app/services/interfaces/cart.service.interface.ts
@@ -1,6 +1,8 @@
 import { Observable } from "rxjs";
 import { Cart } from "../../models/cart.model";
+import { InsertProductIntoCartRequest } from "../../payloads/requests/cart-payloads/insertProductIntoCartRequest";
 
 export interface ICartService {
     getCart(): Observable<Cart>;
+    addItem(item: InsertProductIntoCartRequest): Observable<Cart>;
 }
