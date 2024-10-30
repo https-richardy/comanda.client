bee501b973eb8ba7a9a64494e2536dc234027645
https-richardy <code.richardy@gmail.com>
2024-10-29 22:07:52 -0300

    remove the unused `HTTP_INTERCEPTORS` token from the application

--- source/app/app.tokens.ts
+++ source/app/app.tokens.ts
@@ -1,5 +1,3 @@
-import { HttpInterceptor } from "@angular/common/http";
 import { InjectionToken } from "@angular/core";
 
 export const API_BASE_URL = new InjectionToken<string>("API_BASE_URL");
-export const HTTP_INTERCEPTORS = new InjectionToken<HttpInterceptor>("HTTP_INTERCEPTORS")
