e823a79f9a2341f93c532daca06d83c93ebdbffb
https-richardy <code.richardy@gmail.com>
2024-11-01 20:30:30 -0300

    implements the cart header component, displaying the title “Your Cart” with a gradient style. The component is a standalone component, with the template defined in the file `cart-header.component.html`.

---
+++ source/app/pages/cart/components/cart-header/cart-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cart-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './cart-header.component.html',
+})
+export class CartHeaderComponent {
+
+}
