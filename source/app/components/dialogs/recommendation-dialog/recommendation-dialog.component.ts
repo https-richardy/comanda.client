b4ddc1b1ffcb7756f2dad8f213beb9ec420c9d65
https-richardy <code.richardy@gmail.com>
2024-11-01 16:58:04 -0300

    adds the functionality of generating recommendations on the dialog screen, using a service to query the recommendations API. Displays a spinner while the recommendation is being processed and shows an error message if a problem occurs.

--- source/app/components/dialogs/recommendation-dialog/recommendation-dialog.component.ts
+++ source/app/components/dialogs/recommendation-dialog/recommendation-dialog.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { RecommendationService } from '../../../services/recommendation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-recommendation-dialog',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './recommendation-dialog.component.html'
 })
 export class RecommendationDialogComponent implements OnInit {
+    private readonly recommendationService: RecommendationService;
+
     public recommendation: string = "";
+    public isBusy: boolean = true;
+
+    public constructor(recommendationService: RecommendationService) {
+        this.recommendationService = recommendationService;
+    }
 
     public ngOnInit(): void {
-        this.recommendation = "A geração de recomendação por IA ainda não está implementada! Volte mais tarde.";
+        this.recommendationService
+            .getRecommendation()
+            .subscribe({
+                next: (recommendation) => {
+                    this.recommendation = recommendation.suggestion;
+                    this.isBusy = false;
+                },
+                error: () => {
+                    this.isBusy = false;
+                    this.recommendation = "Ocorreu um erro ao tentar gerar sua recomendação. Tente novamente mais tarde.";
+                }
+            });
     }
 }
