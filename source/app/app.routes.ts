d63274d859be97f6124309d0e7862b13a076ceb9
https-richardy <code.richardy@gmail.com>
2024-11-01 16:19:00 -0300

    this commit introduces the customer's cart page.

--- source/app/app.routes.ts
+++ source/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { ProfilePageComponent } from './pages/profile/profile-page.component';
 import { HomePageComponent } from './pages/home/home.component';
+import { CartPageComponent } from './pages/cart/cart-page.component';
 
 export const routes: Routes = [
     { path: "", component: HomePageComponent },
     { path: "profile", component: ProfilePageComponent },
+    { path: "cart", component: CartPageComponent }
 ];
---
+++ source/app/pages/cart/cart-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../models/cart-item.model';
+import { map, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from "../../layout/navigation/navigation.component";
+
+@Component({
+    selector: 'cart-page',
+    standalone: true,
+    imports: [CommonModule, NavigationComponent],
+    templateUrl: './cart-page.component.html'
+})
+export class CartPageComponent implements OnInit {
+    private readonly cartService: CartService;
+    public items$!: Observable<CartItem[]>;
+
+    public constructor(cartService: CartService) {
+        this.cartService = cartService;
+    }
+
+    public ngOnInit(): void {
+        this.items$ = this.cartService.getCart().pipe(
+            map(cart => cart.items)
+          );
+    }
+
+    public updateQuantity(id: number, change: number) {
+
+    }
+
+    public removeItem(id: number) {
+
+    }
+
+    public getTotal(items: CartItem[]): number {
+        return items.reduce((total, item) => total + (item.price * item.quantity), 0);
+    }
+}
