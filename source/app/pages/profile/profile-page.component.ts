0d4ceacfed9c03e1b8af517b5209cb6b971bc54d
https-richardy <code.richardy@gmail.com>
2024-11-25 13:33:09 -0300

    this change implements the asynchronous loading of user profile information.  The component now uses the `ProfileService` service to fetch the information and store it in `localStorage` for later access.  The display of profile information is now dynamic, displaying data loaded from the service or from local storage

--- source/app/pages/profile/profile-page.component.ts
+++ source/app/pages/profile/profile-page.component.ts
@@ -1,29 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { MainLayoutComponent } from "../../layout/main-layout/main-layout.component";
 import { NavigationComponent } from "../../layout/navigation/navigation.component";
 import { ProfileActionComponent } from './components/profile-action/profile-action.component';
 import { ProfileActionIcons } from './components/profile-action/profile-action.icons';
 import { ProfileInfoComponent } from "./components/profile-info/profile-info.component";
+import { ProfileInformation } from '../../payloads/responses/identity-payloads/profileInformation';
+import { ProfileService } from '../../services/profile.service';
 
 @Component({
     selector: 'app-profile-page',
     standalone: true,
     imports: [
     CommonModule,
-    MainLayoutComponent,
     NavigationComponent,
     ProfileActionComponent,
     ProfileInfoComponent
 ],
     templateUrl: './profile-page.component.html',
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit {
+    private readonly profileService: ProfileService;
+
     public icons = ProfileActionIcons;
-    public user = {
-        name: 'Richard Garcia',
-        email: 'richard@garcia.com',
-        status: 'Ativo'
-    };
+    public user: ProfileInformation = {  } as ProfileInformation;
+
+    public constructor(profileService: ProfileService) {
+        this.profileService = profileService;
+    }
+
+    public ngOnInit(): void {
+        const storedUser = localStorage.getItem("profile.information");
+        if (storedUser) {
+            this.user = JSON.parse(storedUser);
+        }
+
+        else {
+            this.profileService.getProfileInformation()
+                .subscribe((user) => {
+                    this.user = user;
+                    localStorage.setItem("profile.information", JSON.stringify(user));
+                });
+        }
+    }
 }
