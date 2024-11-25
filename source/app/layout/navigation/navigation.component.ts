2226567e36a9edd813daa9a660d06e36661ff53b
https-richardy <code.richardy@gmail.com>
2024-11-25 16:17:21 -0300

    this change implements the variation of the navigation bar interface according to the user's role.

--- source/app/layout/navigation/navigation.component.ts
+++ source/app/layout/navigation/navigation.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavigationItemComponent } from "../navigation-item/navigation-item.component";
 import { CartNavigationItemComponent } from "../cart-navigation-item/cart-navigation-item.component";
+import { AuthorizeViewComponent } from "../../modules/authorization/components/authorize-view/authorize-view.component";
+import { AuthorizedComponent } from "../../modules/authorization/components/authorized/authorized.component";
 
 @Component({
     selector: 'navigation',
     standalone: true,
-    imports: [NavigationItemComponent, CartNavigationItemComponent],
+    imports: [
+        NavigationItemComponent,
+        CartNavigationItemComponent,
+        AuthorizeViewComponent,
+        AuthorizedComponent
+    ],
     templateUrl: './navigation.component.html',
 })
 export class NavigationComponent {
