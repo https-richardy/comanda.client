1ab0b253c59f4a7aa0b891e2712dbf546cb81cf6
https-richardy <code.richardy@gmail.com>
2024-12-02 16:15:39 -0300

    this commit introduces the catalog page

---
+++ source/app/pages/catalog-page/catalog-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainLayoutComponent } from "../../layout/main-layout/main-layout.component";
+
+@Component({
+    selector: 'app-catalog-page',
+    standalone: true,
+    imports: [MainLayoutComponent],
+    templateUrl: './catalog-page.component.html'
+})
+export class CatalogPageComponent {
+
+}
