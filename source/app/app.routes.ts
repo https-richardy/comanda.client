4b0949071e2d2b42e5ccb56a8acb98c145bdbc52
https-richardy <code.richardy@gmail.com>
2024-12-02 16:16:33 -0300

    this change introduces a new route to the catalog page (`/catalog`) and reorganizes the structure of the existing routes. The successful checkout route is now nested under the `/checkout` route, improving the organization and structure of the application's navigation.  The catalog route needs child routes that will be added in a future commit.

--- source/app/app.routes.ts
+++ source/app/app.routes.ts
@@ -4,10 +4,20 @@ import { ProfilePageComponent } from './pages/profile/profile-page.component';
 import { HomePageComponent } from './pages/home/home.component';
 import { CartPageComponent } from './pages/cart/cart-page.component';
 import { SuccessPageComponent } from './pages/checkout/success/success.component';
+import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
 
 export const routes: Routes = [
+    /* register independent routes (standalone pages) here */
     { path: "", component: HomePageComponent },
     { path: "profile", component: ProfilePageComponent },
     { path: "cart", component: CartPageComponent },
-    { path: "checkout/success", component: SuccessPageComponent }
+
+    /* register nested routes (with children) here */
+    {
+        path: "checkout",
+        children: [
+            { path: "success", component: SuccessPageComponent }
+        ]
+    },
+    { path: "catalog", component: CatalogPageComponent } /* TODO: add catalog child routes */
 ];
