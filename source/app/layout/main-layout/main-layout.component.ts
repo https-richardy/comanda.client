1ed68b695e337b0547543ba07ee47d2a6dc49e2c
https-richardy <code.richardy@gmail.com>
2024-12-02 16:37:15 -0300

    this commit introduces a specific layout for users with an administrator profile.  An alternative layout to the main layout (`main-layout`) has been created, adding margins (`ml-20` and `pl-6`) to compensate for the fixed sidebar and ensure correct alignment of the main content.  This avoids overlapping and misalignment of the content in relation to the side navigation for administrators, while keeping the main layout intact for other user profiles.

--- source/app/layout/main-layout/main-layout.component.ts
+++ source/app/layout/main-layout/main-layout.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
 import { AiRecommendationFabComponent } from "../../components/ai-recommendation-fab/ai-recommendation-fab.component";
+import { AuthorizeViewComponent } from "../../modules/authorization/components/authorize-view/authorize-view.component";
+import { AuthorizedComponent } from "../../modules/authorization/components/authorized/authorized.component";
 
 @Component({
     selector: 'main-layout',
     standalone: true,
     imports: [
     NavigationComponent,
-    AiRecommendationFabComponent
+    AiRecommendationFabComponent,
+    AuthorizeViewComponent,
+    AuthorizedComponent
 ],
     templateUrl: './main-layout.component.html'
 })
