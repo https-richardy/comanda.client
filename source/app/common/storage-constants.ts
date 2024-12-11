0114d8a9e41f23648f31b63738998eb88df326b1
https-richardy <code.richardy@gmail.com>
2024-12-11 17:51:53 -0300

    this commit adds a new file `storage-constants.ts` containing a static class `StorageConstants`.  This class defines constants for the keys used in localStorage, centralizing the definition of the keys to avoid duplication and make the code easier to maintain.

---
+++ source/app/common/storage-constants.ts
@@ -0,0 +1,18 @@
+/**
+ * StorageConstants is a static class that contains the keys used in localStorage.
+ * It centralizes key definitions to avoid duplication and facilitate maintenance.
+ */
+export class StorageConstants {
+
+  /**
+   * Key used to store the user's authentication token.
+   * @example 'authentication.token'
+   */
+  public static readonly AuthenticationToken = 'authentication.token';
+
+  /**
+   * Key used to store the logged-in user's information.
+   * @example 'profile.information'
+   */
+  public static readonly ProfileInformation = 'profile.information';
+}
