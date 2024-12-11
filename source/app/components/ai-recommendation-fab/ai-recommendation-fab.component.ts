305fc138cd7f11e2aa4c379197db3cc5c303807a
https-richardy <code.richardy@gmail.com>
2024-12-11 18:59:30 -0300

    refactoring: removes the direct use of SVG from the AI recommendation button icon, for the sake of maintainability and readability.

--- source/app/components/ai-recommendation-fab/ai-recommendation-fab.component.ts
+++ source/app/components/ai-recommendation-fab/ai-recommendation-fab.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DialogService } from '../../services/dialog.service';
 import { RecommendationDialogComponent } from '../dialogs/recommendation-dialog/recommendation-dialog.component';
+import { Icons } from '../../common/enums/icons.enum';
 
 @Component({
     selector: 'recommendation-fab',
@@ -9,6 +10,7 @@ import { RecommendationDialogComponent } from '../dialogs/recommendation-dialog/
     templateUrl: './ai-recommendation-fab.component.html',
 })
 export class AiRecommendationFabComponent {
+    public icons = Icons;
     private readonly dialogService: DialogService;
 
     public constructor(dialogService: DialogService) {
