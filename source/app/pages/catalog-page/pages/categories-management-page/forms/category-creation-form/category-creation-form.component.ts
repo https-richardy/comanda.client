cfbad7bc20f086d91bc67d29b1e745e6d5290cf4
https-richardy <code.richardy@gmail.com>
2024-12-12 21:08:11 -0300

    this commit introduces a form for creating new categories in catalog management.

---
+++ source/app/pages/catalog-page/pages/categories-management-page/forms/category-creation-form/category-creation-form.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter } from '@angular/core';
+import { CategoryCreationRequest } from '../../../../../../payloads/requests/categories-payloads/category-creation.payload';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CategoryService } from '../../../../../../services/category.service';
+
+@Component({
+    selector: 'app-category-creation-form',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        CommonModule
+    ],
+    templateUrl: './category-creation-form.component.html'
+})
+export class CategoryCreationFormComponent {
+    private readonly formBuilder: FormBuilder;
+    private readonly categoryService: CategoryService;
+
+    public payload: CategoryCreationRequest = { title: "" };
+    public form: FormGroup;
+    public onValidSubmit = new EventEmitter<void>;
+
+    public constructor(formBuilder: FormBuilder, categoryService: CategoryService) {
+        this.formBuilder = formBuilder;
+        this.categoryService = categoryService;
+
+        this.form = this.formBuilder.group({
+            title: ["", [Validators.required, Validators.minLength(3)]]
+        });
+    }
+
+    public handleSubmit(): void {
+        if (this.form.valid) {
+            this.payload.title = this.form.get("title")?.value;
+            this.categoryService.createCategory(this.payload).subscribe();
+
+            this.onValidSubmit.emit();
+        }
+    }
+
+    public onCancel(): void {
+        this.form.reset();
+    }
+}
