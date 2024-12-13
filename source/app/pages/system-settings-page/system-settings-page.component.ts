75829537f4f421aeb8a371d3338f0f34519230cf
https-richardy <code.richardy@gmail.com>
2024-12-13 12:18:54 -0300

    this update significantly improves the system settings page, replacing the previous interface with a new responsive and more intuitive structure

--- source/app/pages/system-settings-page/system-settings-page.component.ts
+++ source/app/pages/system-settings-page/system-settings-page.component.ts
@@ -2,15 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../../services/settings.service';
 import { Settings } from '../../models/settings.model';
 import { FormsModule } from '@angular/forms';
+import { TooltipComponent } from "../../components/tooltip/tooltip.component";
+import { MainLayoutComponent } from "../../layout/main-layout/main-layout.component";
+import { Icons } from '../../common/enums/icons.enum';
 
 @Component({
     selector: 'system-settings-page',
     standalone: true,
-    imports: [ FormsModule ],
+    imports: [FormsModule, TooltipComponent, MainLayoutComponent],
     templateUrl: './system-settings-page.component.html'
 })
 export class SystemSettingsPageComponent implements OnInit {
     private readonly settingsService: SettingsService;
+
+    public icons = Icons;
     public settings = {  } as Settings;
 
     public constructor(settingsService: SettingsService) {
@@ -21,5 +26,11 @@ export class SystemSettingsPageComponent implements OnInit {
         this.settingsService.getSettings().subscribe((settings) => {
             this.settings = settings;
         })
+    }
+
+    public handleOnUpdate() {
+        this.settingsService.updateSettings(this.settings).subscribe(() => {
+            alert('Configurações salvas com sucesso!');
+        });
     }
 }
