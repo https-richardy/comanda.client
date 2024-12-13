4edefebad8854a1a00a1328b8747abef6de622e5
https-richardy <code.richardy@gmail.com>
2024-12-12 22:17:57 -0300

    this change implements the functionality for creating and editing categories in category management.  Form components have been added for creation and editing, integration with a service to persist changes in the database and updating the list of categories in the interface after operations have been completed.

--- source/app/pages/catalog-page/pages/categories-management-page/categories-management-page.component.ts
+++ source/app/pages/catalog-page/pages/categories-management-page/categories-management-page.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Category } from '../../../../models/category.model';
 import { CommonModule } from '@angular/common';
 import { DialogService } from '../../../../services/dialog.service';
 import { Icons } from '../../../../common/enums/icons.enum';
 import { MainLayoutComponent } from "../../../../layout/main-layout/main-layout.component";
+import { CategoryCreationFormComponent } from './forms/category-creation-form/category-creation-form.component';
+import { CategoryService } from '../../../../services/category.service';
+import { ChangeDetectorRef } from '@angular/core';
+import { CategoryEditingFormComponent } from './forms/category-editing-form/category-editing-form.component';
 
 @Component({
     selector: 'app-categories-management-page',
@@ -11,23 +15,65 @@ import { MainLayoutComponent } from "../../../../layout/main-layout/main-layout.
     imports: [CommonModule, MainLayoutComponent],
     templateUrl: './categories-management-page.component.html'
 })
-export class CategoriesManagementPageComponent {
+export class CategoriesManagementPageComponent implements OnInit {
     private readonly dialogService: DialogService;
+    private readonly categoryService: CategoryService;
+    private readonly changeDetector: ChangeDetectorRef;
 
     public categories: Category[] = new Array<Category>();
     public currentCategory?: Category
     public icons = Icons;
 
-    public constructor(dialogService: DialogService) {
+    public constructor(
+        dialogService: DialogService,
+        categoryService: CategoryService,
+        changeDetector: ChangeDetectorRef
+    ) {
         this.dialogService = dialogService;
+        this.categoryService = categoryService;
+        this.changeDetector = changeDetector;
+    }
 
-        this.categories.push(new Category(1, "Bebidas"));
-        this.categories.push(new Category(2, "Lanches"));
-        this.categories.push(new Category(3, "Combos"));
+    public ngOnInit(): void {
+        this.categoryService.getAllCategories().subscribe((categories) => {
+            this.categories = categories;
+        });
     }
 
     public openDialog(category?: Category): void {
+        /* if no category is provided, a new one will be created; otherwise, it's for editing */
+        if (!category) {
+            var dialogRef = this.dialogService.open(CategoryCreationFormComponent, {
+                closeOnBackdrop: true,
+                closeOnEscape: true,
+                showCloseButton: false
+            });
 
+            dialogRef.instance.onValidSubmit.subscribe(() => {
+                this.dialogService.close();
+                this.categoryService.getAllCategories().subscribe((categories) => {
+                    this.categories = categories;
+                    this.changeDetector.detectChanges();
+                });
+            });
+        }
+
+        var editingDialogRef = this.dialogService.open(CategoryEditingFormComponent, {
+            closeOnBackdrop: true,
+            closeOnEscape: true,
+            showCloseButton: false,
+            data: {
+                category: category
+            }
+        })
+
+        editingDialogRef.instance.onValidSubmit.subscribe(() => {
+            this.dialogService.close();
+            this.categoryService.getAllCategories().subscribe((categories) => {
+                this.categories = categories;
+                this.changeDetector.detectChanges();
+            })
+        });
     }
 
     public handleDelete(categoryId: number): void {
