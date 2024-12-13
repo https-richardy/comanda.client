b5e734d5cc066b6fa13b82784201e5fc7d93dbf3
https-richardy <code.richardy@gmail.com>
2024-12-13 14:33:22 -0300

    this change implements the `OnInit` interface in the `IngredientManagementPageComponent` component and loads all ingredients via `ingredientService.getAllIngredients()` when initializing the component.

--- source/app/pages/catalog-page/pages/ingredient-management-page/ingredient-management-page.component.ts
+++ source/app/pages/catalog-page/pages/ingredient-management-page/ingredient-management-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MainLayoutComponent } from "../../../../layout/main-layout/main-layout.component";
 import { Ingredient } from '../../../../models/ingredient.model';
 import { Icons } from '../../../../common/enums/icons.enum';
@@ -12,16 +12,29 @@ import { IngredientService } from '../../../../services/ingredient.service';
     imports: [MainLayoutComponent, CommonModule],
     templateUrl: './ingredient-management-page.component.html'
 })
-export class IngredientManagementPageComponent {
+export class IngredientManagementPageComponent implements OnInit {
     private readonly dialogService: DialogService;
     private readonly ingredientService: IngredientService
-    public readonly icons = Icons;
+    private readonly changeDetector: ChangeDetectorRef;
 
+    public icons = Icons;
     public ingredients: Ingredient[] = new Array<Ingredient>();
 
-    public constructor(dialogService: DialogService, ingredientService: IngredientService) {
+    public constructor(
+            dialogService: DialogService,
+            ingredientService: IngredientService,
+            changeDetector:ChangeDetectorRef
+    ) {
         this.dialogService = dialogService;
         this.ingredientService = ingredientService;
+        this.changeDetector = changeDetector;
+    }
+
+    public ngOnInit(): void {
+        this.ingredientService.getAllIngredients().subscribe((ingredients) =>
+        {
+            this.ingredients = ingredients;
+        })
     }
 
     public openDialog(ingredient?: Ingredient): void {
