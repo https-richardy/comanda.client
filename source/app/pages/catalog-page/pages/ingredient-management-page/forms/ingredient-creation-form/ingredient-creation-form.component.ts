3fac1e34c4ea9bbaee78233ae589aa270de4532d
https-richardy <code.richardy@gmail.com>
2024-12-13 21:44:23 -0300

    implements ingredient creation form

---
+++ source/app/pages/catalog-page/pages/ingredient-management-page/forms/ingredient-creation-form/ingredient-creation-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IngredientCreationRequest } from '../../../../../../payloads/requests/ingredients-payloads/ingredient-creation.payload';
+import { IngredientService } from '../../../../../../services/ingredient.service';
+
+@Component({
+    selector: 'ingredient-creation-form',
+    standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        CommonModule
+    ],
+    templateUrl: './ingredient-creation-form.component.html'
+})
+export class IngredientCreationFormComponent {
+    private readonly formBuilder: FormBuilder;
+    private readonly ingredientService: IngredientService;
+
+    public payload: IngredientCreationRequest = { name: "" };
+    public form: FormGroup;
+
+    public onValidSubmit = new EventEmitter<void>();
+    public onCancel = new EventEmitter<void>();
+
+    public constructor(formBuilder: FormBuilder, ingredientService: IngredientService) {
+        this.formBuilder = formBuilder;
+        this.ingredientService = ingredientService;
+
+        this.form = this.formBuilder.group({
+            name: ["", [Validators.required, Validators.minLength(3)]]
+        });
+    }
+
+    public handleSubmit(): void {
+        if (this.form.valid) {
+            this.payload.name = this.form.get("name")?.value;
+            this.ingredientService.createIngredient(this.payload).subscribe();
+
+            this.onValidSubmit.emit();
+        }
+    }
+
+    public handleCancel(): void {
+        this.form.reset();
+        this.onCancel.emit();
+    }
+}
