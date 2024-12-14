0220156bbd7a0de3687699296d0a75188fa4024e
https-richardy <code.richardy@gmail.com>
2024-12-13 21:46:17 -0300

    this commit introduces the functionalities of creating, editing and deleting ingredients

--- source/app/pages/catalog-page/pages/ingredient-management-page/ingredient-management-page.component.ts
+++ source/app/pages/catalog-page/pages/ingredient-management-page/ingredient-management-page.component.ts
@@ -5,6 +5,9 @@ import { Icons } from '../../../../common/enums/icons.enum';
 import { CommonModule } from '@angular/common';
 import { DialogService } from '../../../../services/dialog.service';
 import { IngredientService } from '../../../../services/ingredient.service';
+import { IngredientCreationFormComponent } from './forms/ingredient-creation-form/ingredient-creation-form.component';
+import { IngredientEditingFormComponent } from './forms/ingredient-editing-form/ingredient-editing-form.component';
+import { ConfirmationDialogComponent } from '../../../../components/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
     selector: 'app-ingredient-management-page',
@@ -21,9 +24,9 @@ export class IngredientManagementPageComponent implements OnInit {
     public ingredients: Ingredient[] = new Array<Ingredient>();
 
     public constructor(
-            dialogService: DialogService,
-            ingredientService: IngredientService,
-            changeDetector:ChangeDetectorRef
+        dialogService: DialogService,
+        ingredientService: IngredientService,
+        changeDetector: ChangeDetectorRef
     ) {
         this.dialogService = dialogService;
         this.ingredientService = ingredientService;
@@ -31,17 +34,81 @@ export class IngredientManagementPageComponent implements OnInit {
     }
 
     public ngOnInit(): void {
-        this.ingredientService.getAllIngredients().subscribe((ingredients) =>
-        {
+        this.ingredientService.getAllIngredients().subscribe((ingredients) => {
             this.ingredients = ingredients;
         })
     }
 
     public openDialog(ingredient?: Ingredient): void {
+        let dialogRef;
 
+        if (!ingredient) {
+            dialogRef = this.dialogService.open(IngredientCreationFormComponent, {
+                closeOnBackdrop: true,
+                closeOnEscape: true,
+                showCloseButton: false
+            });
+
+            dialogRef.instance.onValidSubmit.subscribe(() => {
+                this.ingredientService.getAllIngredients().subscribe((ingredients) => {
+                    this.ingredients = ingredients;
+
+                    this.dialogService.close();
+
+                    this.changeDetector.reattach();
+                    this.changeDetector.detectChanges();
+                });
+            });
+
+            dialogRef.instance.onCancel.subscribe(() => {
+                this.dialogService.close();
+            });
+        }
+        else {
+            dialogRef = this.dialogService.open(IngredientEditingFormComponent, {
+                closeOnBackdrop: true,
+                closeOnEscape: true,
+                showCloseButton: false,
+                data: { ingredient }
+            });
+
+            dialogRef.instance.onValidSubmit.subscribe(() => {
+                this.dialogService.close();
+                this.ingredientService.getAllIngredients().subscribe((ingredients) => {
+                    this.ingredients = ingredients;
+                });
+            });
+
+            dialogRef.instance.onCancel.subscribe(() => {
+                this.dialogService.close();
+            });
+        }
     }
 
     public handleDelete(ingredientId: number): void {
+        var dialogRef = this.dialogService.open(ConfirmationDialogComponent, {
+            closeOnBackdrop: true,
+            closeOnEscape: true,
+            showCloseButton: false,
+            data: {
+                title: "Excluir Ingrediente",
+                message: "Você tem certeza que deseja excluir este ingrediente? Esta ação não pode ser desfeita."
+            }
+        });
 
+        dialogRef.instance.onConfirm.subscribe(() => {
+            this.ingredientService.deleteIngredient(ingredientId).subscribe(() => {
+                this.dialogService.close();
+                this.ingredientService.getAllIngredients().subscribe((ingredients) => {
+                    this.ingredients = ingredients;
+                });
+
+                this.changeDetector.detectChanges();
+            });
+        });
+
+        dialogRef.instance.onCancel.subscribe(() => {
+            this.dialogService.close();
+        });
     }
 }
