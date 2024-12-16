bf6e74cf96f34ba652a6235dd5df9d31eac972bc
https-richardy <code.richardy@gmail.com>
2024-12-16 12:03:32 -0300

    this change implements the `createAddtional`, `updateAdditional` and `deleteAdditional` methods in the `IAdditionalService` interface.

--- source/app/services/interfaces/additional.service.interface.ts
+++ source/app/services/interfaces/additional.service.interface.ts
@@ -1,7 +1,12 @@
 import { Observable } from "rxjs";
 import { Additional } from "../../models/additional.model";
+import { AdditionalEditingRequest } from "../../payloads/requests/additionals-payloads/additional-editing.payload";
+import { AdditionalCreationRequest } from "../../payloads/requests/additionals-payloads/additional-creation.payload";
 
 export interface IAdditionalService {
     getAdditionals(): Observable<Additional[]>;
     getAdditionalsByCategory(categoryId: number): Observable<Additional[]>;
+    createAddtional(additional: AdditionalCreationRequest): Observable<void>;
+    updateAdditional(additional: AdditionalEditingRequest): Observable<void>;
+    deleteAdditional(additional: Additional): Observable<void>;
 }
