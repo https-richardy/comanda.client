d460315af75074620c0728d34f02051179e1c39f
https-richardy <code.richardy@gmail.com>
2024-12-16 12:21:26 -0300

    this change implements the new methods defined in the interface. Bringing the functionalities of creating, editing and deleting add-ons to the system

--- source/app/services/additional.service.ts
+++ source/app/services/additional.service.ts
@@ -5,6 +5,8 @@ import { IAdditionalService } from './interfaces/additional.service.interface';
 import { map, Observable } from 'rxjs';
 import { Additional } from '../models/additional.model';
 import { Response } from '../payloads/responses/response';
+import { AdditionalEditingRequest } from '../payloads/requests/additionals-payloads/additional-editing.payload';
+import { AdditionalCreationRequest } from '../payloads/requests/additionals-payloads/additional-creation.payload';
 
 @Injectable({ providedIn: 'root' })
 export class AdditionalService implements IAdditionalService {
@@ -37,6 +39,30 @@ export class AdditionalService implements IAdditionalService {
                 }
 
                 return [];
+            })
+        );
+    }
+
+    public createAddtional(additional: AdditionalCreationRequest): Observable<void> {
+        return this.httpClient.post(this.baseAddress, additional).pipe(
+            map(() => {
+                return void 0;
+            })
+        );
+    }
+
+    public updateAdditional(additional: AdditionalEditingRequest): Observable<void> {
+        return this.httpClient.put(`${this.baseAddress}/${additional.id}`, additional).pipe(
+            map(() => {
+                return void 0;
+            })
+        );
+    }
+
+    public deleteAdditional(additional: Additional): Observable<void> {
+        return this.httpClient.delete(`${this.baseAddress}/${additional.id}`).pipe(
+            map(() => {
+                return void 0;
             })
         );
     }
