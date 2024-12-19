1754635e69ca63ca9aecbc82c3def3e8eed46850
https-richardy <code.richardy@gmail.com>
2024-12-18 22:25:24 -0300

    this change adds a treatment for invalid sessions in the success checkout flow.  Now, if the `sessionId` parameter is not found in the URL, the user will be redirected to the home page.

--- source/app/pages/checkout/success/success.component.ts
+++ source/app/pages/checkout/success/success.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MainLayoutComponent } from "../../../layout/main-layout/main-layout.component";
 import { CheckoutService } from '../../../services/checkout.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { CartService } from '../../../services/cart.service';
 import { OrderConfirmation } from '../../../payloads/responses/checkout-payloads/order-confirmation.payload';
 import { Observable } from 'rxjs';
@@ -17,24 +17,35 @@ export class SuccessPageComponent implements OnInit {
     private readonly checkoutService: CheckoutService;
     private readonly cartService: CartService;
     private readonly route: ActivatedRoute;
+    private readonly routeManager: Router;
+
     public orderInformation = new Observable<OrderConfirmation>;
 
     public constructor(
         checkoutService: CheckoutService,
         cartService: CartService,
-        route: ActivatedRoute
+        route: ActivatedRoute,
+        routeManager: Router
+
     ) {
         this.checkoutService = checkoutService;
         this.cartService = cartService;
         this.route = route;
+        this.routeManager = routeManager;
     }
 
     public ngOnInit(): void {
-        this.processSuccessfulPayment();
+        const sessionId: string = this.route.snapshot.queryParamMap.get('sessionId') as string;
+        if (!sessionId) {
+            this.routeManager.navigate(["/"]);
+            return;
+        }
+
+        this.processSuccessfulPayment(sessionId);
     }
 
-    public processSuccessfulPayment(): void {
-        const sessionId: string = this.route.snapshot.queryParamMap.get('sessionId') as string;
+    public processSuccessfulPayment(sessionId: string): void {
+
         if (sessionId) {
             this.orderInformation = this.checkoutService.handleSuccessfulPayment(sessionId);
             this.orderInformation.subscribe({
