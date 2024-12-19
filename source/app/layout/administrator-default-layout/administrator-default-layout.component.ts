abed16766744123fd0bfb03089f29c609d5f03e2
https-richardy <code.richardy@gmail.com>
2024-12-19 12:56:51 -0300

    injects NotificationService into the administrator's layout component

--- source/app/layout/administrator-default-layout/administrator-default-layout.component.ts
+++ source/app/layout/administrator-default-layout/administrator-default-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
     selector: 'administrator-default-layout',
@@ -10,5 +11,9 @@ import { NavigationComponent } from "../navigation/navigation.component";
     templateUrl: './administrator-default-layout.component.html'
 })
 export class AdministratorDefaultLayoutComponent {
+    private readonly notificationService: NotificationService;
 
+    public constructor(notificationService: NotificationService) {
+        this.notificationService = notificationService;
+    }
 }
