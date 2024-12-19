c5031f0c1942b29ef10f92bad9308cf4eeaaa0d1
https-richardy <code.richardy@gmail.com>
2024-12-18 23:35:12 -0300

    this change introduces authorization restricting the page only to users authenticated as admin.

--- source/app/pages/catalog-page/catalog-page.component.ts
+++ source/app/pages/catalog-page/catalog-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CatalogManagementCardComponent } from "./components/catalog-management-card/catalog-management-card.component";
 import { AdministratorDefaultLayoutComponent } from "../../layout/administrator-default-layout/administrator-default-layout.component";
+import { AuthorizeViewComponent } from "../../modules/authorization/components/authorize-view/authorize-view.component";
+import { AuthorizedComponent } from "../../modules/authorization/components/authorized/authorized.component";
 
 @Component({
     selector: 'app-catalog-page',
@@ -8,8 +10,10 @@ import { AdministratorDefaultLayoutComponent } from "../../layout/administrator-
     templateUrl: './catalog-page.component.html',
     imports: [
         CatalogManagementCardComponent,
-        AdministratorDefaultLayoutComponent
-    ],
+        AdministratorDefaultLayoutComponent,
+        AuthorizeViewComponent,
+        AuthorizedComponent
+],
 })
 export class CatalogPageComponent {
 
