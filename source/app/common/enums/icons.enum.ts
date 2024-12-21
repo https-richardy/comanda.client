c5937ddd1bc8286920b527843aefc74917e95b9e
https-richardy <code.richardy@gmail.com>
2024-12-21 16:13:24 -0300

    adds the shopping_bag, credit_card, check_circle, trending_up, bar_chart and cancel icons to the Icons enum, expanding the options available for use throughout the application.

--- source/app/common/enums/icons.enum.ts
+++ source/app/common/enums/icons.enum.ts
@@ -6,5 +6,11 @@ export enum Icons {
     Delete = "delete",
     Edit = "edit",
     Info = "info",
-    ReceiptLong = "receipt_long"
+    ReceiptLong = "receipt_long",
+    ShoppingBag = "shopping_bag",
+    CreditCard = "credit_card",
+    CheckCircle = "check_circle",
+    TrendingUp = "trending_up",
+    BarChart = "bar_chart",
+    Cancel = "cancel"
 }
