137b52d8651e263bee681e91933a5df8a9ef2b18
https-richardy <code.richardy@gmail.com>
2024-12-21 13:25:21 -0300

    this commit adds a new resolver (`AccessPolicyResolver`) that checks the user's authentication status and redirects to the catalog page (“/catalog”) if you are an administrator, or to the home page (“/home”) if you are a client.

---
+++ source/app/resolvers/access-policy.resolver.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { Resolve, Router } from "@angular/router";
+import { AuthenticationStateProvider } from "../modules/authorization/authenticationStateProvider";
+
+@Injectable({ providedIn: 'root' })
+export class AccessPolicyResolver implements Resolve<void> {
+    private readonly authenticationStateProvider: AuthenticationStateProvider;
+    private readonly routeManager: Router;
+
+    public constructor(authenticationStateProvider: AuthenticationStateProvider, routeManager: Router) {
+        this.authenticationStateProvider = authenticationStateProvider;
+        this.routeManager = routeManager;
+    }
+
+    public resolve(): void {
+        var authenticationState = this.authenticationStateProvider.getAuthenticationState();
+        var claimsIdentity = authenticationState.user;
+
+        if (authenticationState.isAuthenticated() && claimsIdentity?.hasRole("Administrator")) {
+            this.routeManager.navigate(["/catalog"]);
+        }
+
+        if (authenticationState.isAuthenticated() && claimsIdentity?.hasRole("Customer")) {
+            this.routeManager.navigate(["/home"]);
+        }
+    }
+}
