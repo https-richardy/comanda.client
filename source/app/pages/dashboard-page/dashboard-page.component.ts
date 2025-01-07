7da1d8255ca9d9ce3afe78dc6c1e64ad2cf05d2e
https-richardy <code.richardy@gmail.com>
2025-01-07 20:45:00 -0300

    refactor dashboard-page component to include order-panel component

--- source/app/pages/dashboard-page/dashboard-page.component.ts
+++ source/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { SummaryComponent } from './components/summary/summary.component';
 import { AdministratorDefaultLayoutComponent } from "../../layout/administrator-default-layout/administrator-default-layout.component";
+import { OrderPanelComponent } from "../../components/order-panel/order-panel.component";
 
 @Component({
     selector: 'app-dashboard-page',
     templateUrl: './dashboard-page.component.html',
     standalone: true,
-    imports: [SummaryComponent, AdministratorDefaultLayoutComponent],
+    imports: [
+        SummaryComponent,
+        AdministratorDefaultLayoutComponent,
+        OrderPanelComponent
+    ],
 })
 export class DashboardPageComponent {
     public constructor() {
