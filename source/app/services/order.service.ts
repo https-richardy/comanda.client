f6229a0e374b571244079b04f548abf5d131f6cf
https-richardy <code.richardy@gmail.com>
2025-01-07 19:07:20 -0300

    this commit adds a new service (`OrderService`) responsible for fetching requests from the API.

---
+++ source/app/services/order.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { API_BASE_URL } from "../app.tokens";
+import { FetchOrdersRequest } from "../payloads/requests/order-payloads/fetch-orders.payload";
+import { map, Observable } from "rxjs";
+import { FormattedOrder } from "../payloads/responses/order-payloads/formatted-order.payload";
+import { Response } from "../payloads/responses/response";
+import { Pagination } from "../payloads/responses/pagination";
+
+@Injectable({ providedIn: "root" })
+export class OrderService {
+    private readonly httpClient: HttpClient;
+    private readonly baseAddress = `${inject(API_BASE_URL)}api/orders`;
+
+    public constructor(httpClient: HttpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public getOrders(parameters: FetchOrdersRequest): Observable<Array<FormattedOrder>>
+    {
+        var queryParameters = new HttpParams();
+
+        if (parameters.pageNumber) {
+            queryParameters = queryParameters.set("pageNumber", parameters.pageNumber.toString());
+        }
+
+        if (parameters.pageSize) {
+            queryParameters = queryParameters.set("pageSize", parameters.pageSize.toString());
+        }
+
+        if (parameters.status) {
+            queryParameters = queryParameters.set("status", parameters.status.toString());
+        }
+
+        return this.httpClient.get<Response<Pagination<FormattedOrder>>>(this.baseAddress, { params: queryParameters })
+            .pipe(
+                map((response) => {
+                    if (response.data && Array.isArray(response.data)) {
+                        return response.data.results;
+                    }
+
+                    return [ ]
+                })
+            )
+    }
+}
